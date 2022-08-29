b65542a93bc67fb0fbacb0dfe0de64166ace1bc8
prem023 <47606671+prem0023@users.noreply.github.com>
2022-08-29 23:26:48 +0530

    Add files via upload

---
+++ Dating Form/script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("bar-con").classList.toggle("icon");
+    document.getElementById("menu").classList.toggle("change");
+}
---
+++ Restaurant Website/script.js
@@ -0,0 +1,3 @@
+// let height = window.screen.availHeight;
+// let width = window.screen.availWidth;
+// document.body.style.backgroundImage = "url('https://source.unsplash.com/random/wxh')";
